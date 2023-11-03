0d37d97f489833c7aa555510fb05e5ee6d9e9565
HappyPan666 <123617389+HappyPan666@users.noreply.github.com>
2023-11-02 22:44:15 -0700

    Add files via upload
    The basic framework of the Android project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CS5500_Team10"
+include(":app")
+ 
