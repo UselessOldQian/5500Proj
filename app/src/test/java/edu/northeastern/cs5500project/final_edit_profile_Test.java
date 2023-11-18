0ec0d433cc93720bfd3cd72784e75109dbea3c57
craigqian <qian.xingj@northeastern.edu>
2023-11-17 22:24:23 -0800

    add test for edit_profile password can be verified method

---
+++ app/src/test/java/edu/northeastern/cs5500project/final_edit_profile_Test.java
@@ -0,0 +1,38 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FinalEditProfileTest {
+
+  private final_edit_profile editProfile;
+
+  @Before
+  public void setUp() {
+    editProfile = new final_edit_profile();
+  }
+
+  @Test
+  public void testPasswordCanBeVerified_BothPasswordsValid() {
+    assertTrue(editProfile.passwordCanBeVerified("123456", "abcdef"));
+  }
+
+  @Test
+  public void testPasswordCanBeVerified_OldPasswordInvalid() {
+    assertFalse(editProfile.passwordCanBeVerified("1234", "abcdef"));
+  }
+
+  @Test
+  public void testPasswordCanBeVerified_NewPasswordInvalid() {
+    assertFalse(editProfile.passwordCanBeVerified("123456", "abc"));
+  }
+
+  @Test
+  public void testPasswordCanBeVerified_BothPasswordsInvalid() {
+    assertFalse(editProfile.passwordCanBeVerified("123", "ab"));
+  }
+
+  @Test
+  public void testPasswordCanBeVerified_BothPasswordsEmpty() {
+    assertFalse(editProfile.passwordCanBeVerified("", ""));
+  }
+}
